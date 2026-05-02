f9efde0a83ccbd7e3f2b4d2b1118d6526f64d02e
no53-2525 <noenokienoki.503125@gmail.com>
2026-05-03 08:47:41 +0900

    Add files via upload

---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,33 @@
+package kadai_021;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+	HashMap<String, String> fruit = new HashMap<String, String>();
+	
+	public Dictionary_Chapter21() {
+		fruit.put("apple", "りんご");
+		fruit.put("peach", "桃");
+		fruit.put("banana", "バナナ");
+		fruit.put("lemon", "レモン");
+		fruit.put("pear", "梨");
+		fruit.put("kiwi", "キウイ");
+		fruit.put("straeberry", "いちご");
+		fruit.put("grape", "ぶどう");
+		fruit.put("muscat", "マスカット");
+		fruit.put("cherry", "さくらんぼ");
+		
+	}
+	
+	public void searchWords(String[] searchWords) {
+		for(int i =0; i <searchWords.length; i++) {
+			String word = searchWords[i];
+			if(fruit.containsKey(word)) {
+				System.out.println(word+"の意味は"+fruit.get(word)+"です。");
+			}else {
+				System.out.println(word+"辞書に登録されていません。");
+			}
+		}
+		
+	}
+}

b0b936c6d71960e6f46b70df8ccbbe3bd11e988e
no53-2525 <noenokienoki.503125@gmail.com>
2026-05-03 09:13:31 +0900

    Add files via upload

--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -12,7 +12,7 @@ public class Dictionary_Chapter21 {
 		fruit.put("lemon", "レモン");
 		fruit.put("pear", "梨");
 		fruit.put("kiwi", "キウイ");
-		fruit.put("straberry", "いちご");
+		fruit.put("straｗberry", "いちご");
 		fruit.put("grape", "ぶどう");
 		fruit.put("muscat", "マスカット");
 		fruit.put("cherry", "さくらんぼ");
@@ -25,7 +25,7 @@ public class Dictionary_Chapter21 {
 			if(fruit.containsKey(word)) {
 				System.out.println(word+"の意味は"+fruit.get(word));
 			}else {
-				System.out.println(word+"は辞書に登録されていません。");
+				System.out.println(word+"は辞書に存在しません。");
 			}
 		}
 		
